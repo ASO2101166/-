9c70e8f5af09fbe2818a64c3d0610519a1dd2e0c
志水 <115598372+ASO2101166@users.noreply.github.com>
2022-11-14 14:48:07 +0900

    CartUpdate

--- CartUpdate.js
+++ CartUpdate.js
@@ -1,1 +1,15 @@
-
+aaaaaaa.addEventListener('click', function() {
+    let data = document.querySelector('zouka').innerHTML;
+    fetch('./CartUpdate/php', {
+        method: "POST",
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data)
+    })
+    .then(response => response.json()) // 返ってきたレスポンスをjsonで受け取って次のthenへ渡す
+    .then(res => {
+        console.log(res); // やりたい処理
+    })
+    .catch(error => {
+        console.log(error); // エラー表示
+    });
+})
--- createcarts.sql
+++ createcarts.sql
@@ -1,9 +1,9 @@
 --カートテーブル作成--
 CREATE TABLE carts
-(cart_id      CHAR(8), -- カートID
- user_id    CHAR(8) NOT NULL, -- 会員番号
- item_id CHAR(8) NOT NULL, -- 商品ID
- quantity     INT NOT NULL, -- 数量
+(cart_id                  CHAR(8), -- カートID
+ user_id                  CHAR(8) NOT NULL, -- 会員番号
+ item_id                  CHAR(8) NOT NULL, -- 商品ID
+ quantity                 INT NOT NULL, -- 数量
  registration_status      BOOLEAN NOT NULL, -- 登録情報フラグ
  PRIMARY KEY (user_id)
  FOREIGN KEY user_id REFERENCES users(user_id)
