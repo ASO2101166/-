e9c20aaa8ccf1e55802879ceb399fc92a4410c8b
ASO2201115 <114889731+ASO2201115@users.noreply.github.com>
2022-11-14 10:41:08 +0900

    Add files via upload

---
+++ createorders.sql
@@ -0,0 +1,8 @@
+--注文テーブル作成--
+CREATE TABLE orders
+(order_id      CHAR(12), --注文番号 
+ user_id CHAR(8) NOT NULL, -- 会員番号
+ total_price INT NOT NULL, -- 合計金額
+ PRIMARY KEY (order_id)
+ FOREIGN KEY user_id REFERENCES users(user_id)
+);
