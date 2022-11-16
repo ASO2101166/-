0de040df3aba5892cddcb9f17a59775feecf0273
ASO2101137 <2101137@s.asojuku.ac.jp>
2022-11-16 11:01:07 +0900

    ログイン画面
    修正中

--- Login.php
+++ Login.php
@@ -1,3 +1,8 @@
-<?php
-echo 'あ';
-?>
+<h1>学生のためのスポーツ用品</h1>
+<link rel="stylesheet" href="./css/login_style.css" />
+<form action="LoginCheck.php" method="post">
+    <label>メールアドレス</label>　<input class="a" type="text" mame="mail"><br>
+    <br>
+    <label>パスワード</label>　<input class="a" type="text" mame="pass">
+    <input id="b" type="submit" value="ログイン">
+</form>
