be580bce823824d8f3f392a6fa9d9b91fa6587e8
ASO2201115 <114889731+ASO2201115@users.noreply.github.com>
2022-11-16 09:47:59 +0900

    Add files via upload

---
+++ createorderdetails.sql
@@ -0,0 +1,12 @@
+--注文明細テーブル作成--
+CREATE TABLE orderdetails
+(order_id      CHAR(12), -- 注文番号
+ order_detail_id    CHAR(3) NOT NULL, -- 注文明細番号
+ item_id CHAR(8) NOT NULL, -- 商品ID
+ quantity     INT NOT NULL, -- 数量
+ estimated_delivery_date      DATE NOT NULL, -- 配送予定日
+ PRIMARY KEY (order_id),
+ PRIMARY KEY (order_detail_id),
+ FOREIGN KEY order_id REFERENCES orders(order_id),
+ FOREIGN KEY item_id REFERENCES items(item_id)
+);
