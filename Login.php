486df4712a8560b807ad5bf9524aee3ca6eb4493
ASO2101137 <2101137@s.asojuku.ac.jp>
2022-11-18 12:14:51 +0900

    ログイン画面完成（仮）
    デザインは後で決め、変更する

--- Login.php
+++ Login.php
@@ -1,8 +1,9 @@
-<h1>学生のためのスポーツ用品</h1>
+<br><h1>学生のためのスポーツ用品</h1><br>
 <link rel="stylesheet" href="./css/login_style.css" />
 <form action="LoginCheck.php" method="post">
-    <label>メールアドレス</label>　<input class="a" type="text" mame="mail"><br>
+    <label>メールアドレス　</label> <input class="a" type="text" mame="mail"><br>
     <br>
-    <label>パスワード</label>　<input class="a" type="text" mame="pass">
-    <input id="b" type="submit" value="ログイン">
+    <br>
+    <label>パスワード　</label> <input class="a" type="text" mame="pass"><br>
+    <input id="btn" type="submit" value="ログイン"><br>
 </form>
