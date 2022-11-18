6974be8ce2f6e465e89e38a5921747393d7c1a05
志水 <115598372+ASO2101166@users.noreply.github.com>
2022-11-18 12:11:48 +0900

    Update CreateItems.sql

--- CreateItems.sql
+++ CreateItems.sql
@@ -5,7 +5,7 @@ CREATE TABLE items
  item_image            VARCHAR(255) -- 商品画像
  explanation           TEXT, -- 説明
  unit_price            INT NOT NULL, -- 単価
- genre_id              CHAR(5) NOT NULL, -- ジャンルID
+ genre_code            CHAR(6) NOT NULL, -- ジャンルID
  set_discount_division CHAR(5), -- セット割引区分
  PRIMARY KEY (item_id),
  FOREIGN KEY genre_id REFERENCES genres(genre_id)
