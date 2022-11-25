7ce596409cf5b127ca529f090c27059664907a08
志水 <115598372+ASO2101166@users.noreply.github.com>
2022-11-25 10:08:29 +0900

    Update CreateItems.sql

--- sql/CreateItems.sql
+++ sql/CreateItems.sql
@@ -1,6 +1,6 @@
 --商品テーブル作成--
 CREATE TABLE items
-(item_id               CHAR(8), -- 商品ID
+(item_id               INT AUTO_INCREMENT, -- 商品ID
  item_name             VARCHAR(255) NOT NULL, -- 商品名
  item_image            VARCHAR(255) -- 商品画像
  explanation           TEXT, -- 説明
