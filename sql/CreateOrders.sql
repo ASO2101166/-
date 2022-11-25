eaf1f4e5df0f9e9977ddf7ffd5c8d95e0c1a0d69
志水 <115598372+ASO2101166@users.noreply.github.com>
2022-11-25 11:14:13 +0900

    Update CreateOrders.sql

--- sql/CreateOrders.sql
+++ sql/CreateOrders.sql
@@ -1,6 +1,6 @@
---注文テーブル作成--
+-- 注文テーブル作成-- 
 CREATE TABLE orders
-(order_id    CHAR(12), --注文番号 
+(order_id    CHAR(12), -- 注文番号 
  user_id     INT NOT NULL, -- 会員番号
  total_price INT NOT NULL, -- 合計金額
  PRIMARY KEY (order_id)
