0cf83411454bd8c920504e90845b79339f7bf617
志水 <115598372+ASO2101166@users.noreply.github.com>
2022-11-25 10:09:57 +0900

    Update CreateOrderDetails.sql

--- sql/CreateOrderDetails.sql
+++ sql/CreateOrderDetails.sql
@@ -1,8 +1,8 @@
 --注文明細テーブル作成--
 CREATE TABLE orderdetails
 (order_id                CHAR(12), -- 注文番号
- order_detail_id         CHAR(3) NOT NULL, -- 注文明細番号
- item_id                 CHAR(8) NOT NULL, -- 商品ID
+ order_detail_id         INT AUTO_INCREMENT NOT NULL, -- 注文明細番号
+ item_id                 INT NOT NULL, -- 商品ID
  quantity                INT NOT NULL, -- 数量
  estimated_delivery_date DATE NOT NULL, -- 配送予定日
  PRIMARY KEY (order_id),
