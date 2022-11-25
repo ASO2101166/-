bed5f069e21ac7d855dd479599c910bd0c51c8f7
志水 <115598372+ASO2101166@users.noreply.github.com>
2022-11-25 11:09:09 +0900

    Update CreateGenres.sql

--- sql/CreateGenres.sql
+++ sql/CreateGenres.sql
@@ -4,6 +4,6 @@ CREATE TABLE genres
  genre_name        VARCHAR(30) NOT NULL, -- ジャンル名
  genre_level       INT NOT NULL, -- 階層
  parent_genre_code CHAR(6), -- 親ジャンルID
- PRIMARY KEY (genre_id),
- FOREIGN KEY (parent_genre_id) REFERENCES genres(genre_id)
+ PRIMARY KEY (genre_code),
+ FOREIGN KEY (parent_genre_code) REFERENCES genres(genre_code)
 );
