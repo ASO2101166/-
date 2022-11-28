6273ee764ef1743a4286c9f8ef816b1977501954
志水 <115598372+ASO2101166@users.noreply.github.com>
2022-11-28 10:28:50 +0900

    Create InsertUser_input.php

---
+++ sql/InsertUser_input.php
@@ -0,0 +1,7 @@
+<form action="InsertUserTable.php" method="post">
+    <input type="text" name="name">
+    <input type="mail" name="mail">
+    <input type="password" name="password">
+    <input type="text" name="address">
+    <input type="submit" value="登録">
+</form>
