34144bc4c723cc9b7fbde301b8e762a8e0960ea4
志水 <115598372+ASO2101166@users.noreply.github.com>
2022-11-28 10:30:08 +0900

    InsertUserTable

--- Dbconect.php
+++ Dbconect.php
@@ -1,1 +1,8 @@
-
+<?php
+    class Dbconect{
+        public function dbConnect(){
+            $pdo = new PDO('mysql:host=localhost;dbname=ecsite;charset=utf8','webuser','abccsd2');
+            return $pdo;
+        }
+    }
+?>
--- sql/InsertUserTable.php
+++ sql/InsertUserTable.php
@@ -1,1 +1,14 @@
+<?php
+    require_once '../Dbconect.php';
 
+    $cls = new Dbconect();
+    $pdo = $cls->dbConect();
+    $sql = 'INSERT INTO users(user_name, mail_address, password, address, point) VALUES (?,?,?,?,0);';
+
+    $ps = $pdo->prepare($sql);
+    $ps->bindValue(1, $_POST['name'], PDO::PARAM_STR);
+    $ps->bindValue(2, $_POST['mail'], PDO::PARAM_STR);
+    $ps->bindValue(3, password_hash($_POST['pass'], PASSWORD_DEFAULT), PDO::PARAM_STR);
+    $ps->bindValue(4, $_POST['address'], PDO::PARAM_STR);
+    $ps->execute();
+?>
