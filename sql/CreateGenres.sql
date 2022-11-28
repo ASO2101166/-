4e843fe028ed97a1604a9418321980f13d58641c
志水 <115598372+ASO2101166@users.noreply.github.com>
2022-11-28 12:02:54 +0900

    Delete CreateGenres.sql

--- sql/CreateGenres.sql
+++
@@ -1,9 +0,0 @@
--- ジャンルテーブル作成-- 
-CREATE TABLE genres
-(genre_code        CHAR(6), -- ジャンルID
- genre_name        VARCHAR(30) NOT NULL, -- ジャンル名
- genre_level       INT NOT NULL, -- 階層
- parent_genre_code CHAR(6), -- 親ジャンルID
- PRIMARY KEY (genre_code),
- FOREIGN KEY (parent_genre_code) REFERENCES genres(genre_code)
-);
