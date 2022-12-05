b9464edb1e0a305bba08a81dbabca4ddea2d08cc
志水 <115598372+ASO2101166@users.noreply.github.com>
2022-12-05 14:17:40 +0900

    注文履歴INSERT処理

--- php/OrderCreate.php
+++ php/OrderCreate.php
@@ -19,11 +19,27 @@
     $ps->bindValue(1,$user->user_id,PDO::PARAM_INT);
     $ps->execute();
 
-    $sql2 = "INSERT INTO";
+    $sql2 = "INSERT INTO orderdetails(order_id, order_detail_id, item_id, quantity, estimated_delivery_date) VALUES
+             ((SELECT MAX(order_id) FROM orders),
+             0,
+             (SELECT item_id FROM carts WHERE cart_id = ?),
+             (SELECT quantity FROM carts WHERE cart_id = ?),
+             DATE_ADD(now(), INTERVAL 7 DAY));";
+
+    $sql3 = "UPDATE orderdetails SET order_detail_id = (SELECT MAX(order_detail_id) + 1 
+                                                        FROM orderdetails 
+                                                        WHERE order_id = (SELECT MAX(order_id) FROM orders))
+             WHERE order_detail_id = 0
+    ;";
+
+
     foreach($_POST['cart_id'] as $cart_id){
-        
+        $ps = $pdo->prepare($sql2);
+        $ps->bindValue(1,$cart_id,PDO::PARAM_INT);
+        $ps->bindValue(2,$cart_id,PDO::PARAM_INT);
+        $ps->execute();
+        $pdo->query($sql3);
     }
 
-
-
+    header('Location: ../OrderHistory.html');
 ?>
