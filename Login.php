e34b5fcd71f9af007eb47c640ec0b65742c4bea5
ASO2101153 <114889457+ASO2101153@users.noreply.github.com>
2022-12-05 09:31:47 +0900

    ログイン画面の変更

--- Login.php
+++ Login.php
@@ -1,9 +1,9 @@
-<br><h1>学生のためのスポーツ用品</h1><br>
-<link rel="stylesheet" href="./css/login_style.css" />
-<form action="LoginCheck.php" method="post">
-    <label>メールアドレス　</label> <input class="a" type="text" name="mail"><br>
-    <br>
-    <br>
-    <label>パスワード　</label> <input class="a" type="text" name="password"><br>
-    <input id="btn" type="submit" value="ログイン"><br>
-</form>
+    <h1>学生のためのスポーツ用品</h1>
+    <link rel="stylesheet" href="./css/login_style.css" />
+    <div class="allform">
+    <form action="LoginCheck.php" method="post">
+        <input class="form" type="text" name="mail" placeholder="mail address"><br>
+        <input class="form" type="text" name="password" placeholder="password">
+        <input id="btn" type="submit" value="ログイン">
+    </form>
+    </div>
