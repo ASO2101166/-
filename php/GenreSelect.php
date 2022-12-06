0982afd58acb694e3367eab3d3b2a18548744b44
志水 <115598372+ASO2101166@users.noreply.github.com>
2022-12-06 13:33:17 +0900

    ジャンル選択php処理完成

--- php/GenreSelect.php
+++ php/GenreSelect.php
@@ -8,16 +8,16 @@
             $genreData = $pdo->query($sql);
             return $genreData;
         }
-        // function genreselect(){
-        //     $cls = new Dbconnect();
-        //     $pdo = $cls->dbConnect();
-        //     $sql = "SELECT * FROM genres AS P INNER JOIN genres AS C
-        //             WHERE ;";
-        //     $ps = $pdo->prepare($sql);
-        //     $ps->bindValue(1,$mail,PDO::PARAM_STR);
-        //     $ps->execute();
-        //     $genreData = $ps->fetchAll();
-        //     return $genreData;
-        // }
+
+        function genreselectlevel2($parent_genre_code){
+            $cls = new Dbconnect();
+            $pdo = $cls->dbConnect();
+            $sql = "SELECT * FROM genres WHERE parent_genre_code = ?;";
+            $ps = $pdo->prepare($sql);
+            $ps->bindValue(1,$parent_genre_code,PDO::PARAM_STR);
+            $ps->execute();
+            $genreData = $ps->fetchAll();
+            return $genreData;
+        }
     }
 ?>
