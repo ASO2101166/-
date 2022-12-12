7b8d3c5d4451fa3921d50f6745f2e5a9ce3c5248
志水 <115598372+ASO2101166@users.noreply.github.com>
2022-12-12 15:45:42 +0900

    もっと見るボタン初期表示

--- php/ItemSelect.php
+++ php/ItemSelect.php
@@ -9,10 +9,6 @@
             $pdo = $cls->dbConnect();
             $sql = "SELECT * FROM items LIMIT 9;";
             $itemData = $pdo->query($sql);
-            // $ps = $pdo->prepare($sql);
-            // $ps->bindValue(1,$mail,PDO::PARAM_STR);
-            // $ps->execute();
-            // $userData = $ps->fetchAll();
             return $itemData;
         }
 
@@ -22,6 +18,40 @@
             $sql = "SELECT * FROM items WHERE genre_code = ? LIMIT 9;";
             $ps = $pdo->prepare($sql);
             $ps->bindValue(1,$genre_code,PDO::PARAM_STR);
+            $ps->execute();
+            $itemData = $ps->fetchAll();
+            return $itemData;
+        }
+
+        function itemselectlimit(){
+            $cls = new Dbconnect();
+            $pdo = $cls->dbConnect();
+            $sql = "
+            SELECT CASE
+                      WHEN Btbl.bcnt = tbl.cnt THEN true
+                      ELSE false
+                   END AS 'CHECK'
+            FROM (SELECT COUNT(*) AS bcnt FROM items) AS Btbl,
+                 (SELECT COUNT(*) AS cnt FROM (SELECT * FROM items LIMIT 9) AS lmttbl) AS tbl;
+                    ";
+            $itemData = $pdo->query($sql);
+            return $itemData;
+        }
+
+        function itemselectlimitBygenre($genre_code){
+            $cls = new Dbconnect();
+            $pdo = $cls->dbConnect();
+            $sql = "
+            SELECT CASE
+                      WHEN Btbl.bcnt = tbl.cnt THEN true
+                      ELSE false
+                   END AS 'CHECK'
+            FROM (SELECT COUNT(*) AS bcnt FROM items WHERE genre_code = ?) AS Btbl,
+                 (SELECT COUNT(*) AS cnt FROM (SELECT * FROM items WHERE genre_code = ? LIMIT 9) AS lmttbl) AS tbl;
+                    ";
+            $ps = $pdo->prepare($sql);
+            $ps->bindValue(1,$genre_code,PDO::PARAM_STR);
+            $ps->bindValue(2,$genre_code,PDO::PARAM_STR);
             $ps->execute();
             $itemData = $ps->fetchAll();
             return $itemData;
